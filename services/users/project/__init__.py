ba5d1ff7fabeb9d4d25170e35de28bc471ae912e
mycok <awesomeme155@gmail.com>
2020-02-02 01:35:49 +0300

    chore: initial-project-setup
    - install flask
    - setup docker and docker-compose
    - setup the users microservice
    
    [Finishes #168649989]

---
+++ services/users/project/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!!'
+    })
